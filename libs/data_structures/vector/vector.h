210f5bf0e92a8380cee0fb1a80d0d2c6335720a8
icaasy <yrukdrobotenko@gmail.com>
2024-03-21 11:55:39 +0300

    add vector library

---
+++ libs/data_structures/vector/vector.h
@@ -0,0 +1,11 @@
+#ifndef INC_VECTOR_H
+#define INC_VECTOR_H
+
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+#endif
---
+++ main.c
@@ -0,0 +1,3 @@
+//
+// Created by iyruk on 21.03.2024.
+//
