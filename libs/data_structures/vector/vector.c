317ea1b940547d123bc5dd2da99ddee3cebc0516
icaasy <yrukdrobotenko@gmail.com>
2024-03-21 11:58:03 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,3 +1,38 @@
-//
-// Created by iyruk on 21.03.2024.
-//
+#include <data_structures/vector/vector.h>
+
+
+vector createVector(size_t n){
+    vector new;
+    new.data = (int *) malloc(n * sizeof(int));
+    new.size = 0;
+    new.capacity = n;
+
+    if (!new.data){
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+
+    return new;
+}
+
+
+void reserve(vector *v, size_t newCapacity){
+    v->data = (int *)realloc(v->data, sizeof(int) * newCapacity);
+    v->size = v->size > newCapacity ? newCapacity : v->size;
+    v->capacity = newCapacity;
+}
+
+
+void shrinkToFit(vector *v){
+    if (v->capacity > v->size){
+        v->data = (int *)realloc(v->data, sizeof(int) * v->size);
+        v->capacity = v->size;
+    }
+}
+
+
+void deleteVector(vector *v){
+    free(v->data);
+    v->size = 0;
+    v->capacity = 0;
+}
